2a9012273172a5ae40ca4d7be7db9cfdb18408c7
Win Feigle <win.feigle@gmail.com>
2022-05-18 21:02:09 -0600

    commit tree b662d871e0c91af775223deca18b6fa151ac3c8b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (arr, eventName) => {
+    const newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArr;
+}
+
+const countDown = x => {
+    while(x >= 0){
+        console.log(x);
+        x--;
+    }
+}
